233de260198a15b8f9ed8f7b0665a8c16f792e15
sayo18 <sayo18biblio@gmail.com>
2018-05-18 09:43:19 +0200

    Versió 0.2 del projecte prjava01

--- src/prjava01/Prjava01.java
+++ src/prjava01/Prjava01.java
@@ -20,6 +20,7 @@ public class Prjava01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
+        
         File f = new File("fitxer.html");
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
             bw.write("<html>");
@@ -35,6 +36,7 @@ public class Prjava01 {
             bw.write("  </head>");
             bw.newLine();
             bw.write("  <body>");
+            bw.write("    <h1>DAW2 m08uf4pr2</h1>");bw.newLine();
             bw.newLine();
             bw.write("    Nova p&agrave;gina web");
             bw.newLine();
